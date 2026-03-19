608e00640d4569e50dd4267f71b20a85eff1ff4e
CSQ <csqsaysuwu@gmail.com>
2026-03-20 02:24:30 +0800

    hw0104

--- kernel/main.c
+++ kernel/main.c
@@ -15,7 +15,7 @@ main()
     printfinit();
     printf("\n");
     printf("xv6 kernel is booting\n");
-    printf("\n");
+    printf("hart %d starting\n", cpuid());
     kinit();         // physical page allocator
     kvminit();       // create kernel page table
     kvminithart();   // turn on paging
